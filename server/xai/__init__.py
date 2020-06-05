bfd54ca226c2150be0529c8ac57708023bfa5915
Up Chen <up@vultr.guest>
2020-06-05 21:00:32 +0800

    Support static file supporting.

--- server/xai/__init__.py
+++ server/xai/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_sockets import Sockets
 # from pymongo import MongoClient
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static', static_folder='static')
 app.wsgi_app = ProxyFix(app.wsgi_app)
 app.config.from_pyfile('../settings.py')
 SETTINGS = app.config
